c7f9d990571cbeb87dfc2eaec6d26ef05d0396e4
harryo583 <hongyiwang2027@u.northwestern.edu>
2024-12-03 20:06:23 -0600

    basic implementation working

--- app.js
+++ app.js
@@ -16,11 +16,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
-// Use Routes
+// Use routes
 app.use('/', indexRouter);
 app.use('/upload', uploadRouter);
 
-// Start the server
+// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
